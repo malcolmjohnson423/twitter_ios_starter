a6a339adb71c707260f95b4ffa5c06e1ce110a2c
Malcolm Johnson <malcolm.johnson423@gmail.com>
2021-03-07 16:33:31 -0500

    added retweet and favorite to tweets

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -19,8 +19,19 @@ class HomeTableViewController: UITableViewController {
         super.viewDidLoad()
         loadTweets()
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
-        tableView.refreshControl = myRefreshControl
+        self.tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
     }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
+    
+    
+    
     @objc func loadTweets(){
         NumberofTweets = 20
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
@@ -81,6 +92,11 @@ class HomeTableViewController: UITableViewController {
             cell.profilePic.image = UIImage(data: imageData)
         }
         
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+    
         
         return cell
     }
